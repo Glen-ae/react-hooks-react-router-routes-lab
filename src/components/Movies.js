b8a2acea4afcec01d6655963479ecb1443685df7
Glen <glen.kimani@student.moringaschool.com>
2023-01-20 14:22:48 +0300

    Completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,19 @@
-import React from "react";
+import Movies from "./Movies"
 import { movies } from "../data";
+import Movie from "./Movie"
+
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const moviesList=movies.map(movie =>
+     <Movie movie={movie} key={movie.title}/>)
+  
+     
+  return(
+   <div>
+    <h1>Movies Page</h1>
+    {moviesList}
+  </div>
+  );
 }
 
 export default Movies;
